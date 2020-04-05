21636487e26fe22ffd870c0460ec616b2a9176aa
Deepanshu Rohilla <drohilla@mphrx.com>
2020-04-06 03:48:35 +0530

    json responses return from controller, inn place of jsp responses

--- src/com/deepanshu/controllers/HelloController.java
+++ src/com/deepanshu/controllers/HelloController.java
@@ -1,23 +1,28 @@
 package com.deepanshu.controllers;
 
+import com.deepanshu.pojos.Demo;
 import org.json.HTTP;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.ws.http.HTTPBinding;
 import java.io.BufferedReader;
 
-@Controller
+@RestController
 @RequestMapping("/controllerMap")
 public class HelloController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.POST)
-    public String  demoFunc(HttpServletRequest request){
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public Demo demoFunc(HttpServletRequest request){
 
         StringBuffer jb = new StringBuffer();
         String line = null;
@@ -33,8 +38,6 @@ public class HelloController {
             System.out.println("exception occured while parssing");
         }
 
-        return "demo";
-
-
+        return new Demo(1,2,3,5);
     }
 }
