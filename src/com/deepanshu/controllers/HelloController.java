e11d08822eddd76c6653a423203f45f1e213bdfd
Deepanshu Rohilla <drohilla@mphrx.com>
2020-04-06 11:31:06 +0530

    Revert "@RequestBody annotation for request Binding"
    This reverts commit 2508424c902dfa2f54b1749577b75046b8a75e75.

--- src/com/deepanshu/controllers/HelloController.java
+++ src/com/deepanshu/controllers/HelloController.java
@@ -3,6 +3,9 @@ package com.deepanshu.controllers;
 import com.deepanshu.pojos.Demo;
 import com.deepanshu.pojos.Demo1;
 import com.deepanshu.pojos.Demo2;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.HTTP;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -15,6 +18,7 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.ws.http.HTTPBinding;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
@@ -23,25 +27,44 @@ import java.io.InputStreamReader;
 public class HelloController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.POST)
-    public Demo demoFunc(HttpServletRequest request, @RequestBody Demo demo){
+    public Demo demoFunc(HttpServletRequest request){
 
 //        System.out.println(demo);
-//        StringBuffer jb = new StringBuffer();
-//        String line = null;
-//        try {
-//            InputStream in = request.getInputStream();
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-//            while ((line = reader.readLine()) != null)
-//                jb.append(line);
-//        } catch (Exception e) { /*report an error*/
-//            System.out.println(e);}
-//        System.out.println("deepu " + jb.toString());
-//        try{
-//            JSONObject obj = new JSONObject(jb.toString());
-//        }catch(JSONException ex){
-//            System.out.println("exception occured while parssing");
-//        }
+        StringBuffer jb = new StringBuffer();
+        String line = null;
+        try {
+            InputStream in = request.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            while ((line = reader.readLine()) != null)
+                jb.append(line);
+        } catch (Exception e) { /*report an error*/
+            System.out.println(e);}
+        System.out.println("deepu " + jb.toString());
+        try{
+            JSONObject obj = new JSONObject(jb.toString());
 
-        return demo;
+            ObjectMapper om = new ObjectMapper();
+            om.readValue(obj.get("demoVar").toString(), Demo1.class);
+
+        }catch(JSONException ex){
+            System.out.println("exception occured while parssing");
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Demo(1,2,3,4, new Demo1(3,4,5));
     }
 }
+
+
+
+
+
+
+
+//  TODO : make a generic JSON parser from request body
+//  TODO : make a generic utility method for object mapping
